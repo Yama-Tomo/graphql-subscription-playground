bc4be05c55b18e77ddf704f2653e7ff37a5d4a65
Yama-Tomo <yam4.tomo@gmail.com>
2021-12-06 12:50:52 +0900

    useMutationをapollo-clientライクに記述できるように高階関数を作成した

--- packages/frontend/src/hooks/api/adapter.ts
+++ packages/frontend/src/hooks/api/adapter.ts
@@ -1,4 +1,10 @@
-import { UseQueryResponse } from 'urql';
+import {
+  OperationContext,
+  OperationResult,
+  UseMutationResponse,
+  UseMutationState,
+  UseQueryResponse,
+} from 'urql';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const toApolloClientIFUseQuery = <T extends (args: any) => UseQueryResponse>(useQueryFn: T) => {
@@ -19,6 +25,43 @@ const toApolloClientIFUseQuery = <T extends (args: any) => UseQueryResponse>(use
   };
 };
 
+type WrappedExecute<Data = unknown, Variables = unknown> = (
+  executeArgs: { variables: Variables } & Partial<OperationContext>
+) => Promise<OperationResult<Data, Variables>>;
+
+type CustomResult<Data = unknown, Variables = unknown> = Pick<
+  UseMutationState<Data, Variables>,
+  'data' | 'error'
+> & { loading: boolean };
+
+type UseMutationWrapperReturnType<T> = T extends (
+  options: unknown
+) => UseMutationResponse<infer Data, infer Variables>
+  ? [WrappedExecute<Data, Variables>, CustomResult]
+  : never;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const toApolloClientIFUseMutation = <T extends (args: unknown) => UseMutationResponse<any, any>>(
+  useMutationFn: T
+) => {
+  return function useMutationWrapper(...options: Parameters<T>): UseMutationWrapperReturnType<T> {
+    const [res, execute] = useMutationFn(options);
+
+    const wrappedExecute: WrappedExecute = ({ variables, ...context }) =>
+      execute(variables, context);
+
+    const result: CustomResult = {
+      data: res.data,
+      loading: res.fetching,
+      error: res.error,
+    };
+
+    // この関数の戻り値がconditional typeでnever型に合わせることができないのでanyで強制的にキャスト
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return [wrappedExecute, result] as any;
+  };
+};
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const swapOptKeys = <T extends Record<string, any>>(options: T) => {
   const swapKeys: Record<string, string> = {
@@ -32,4 +75,4 @@ const swapOptKeys = <T extends Record<string, any>>(options: T) => {
   }, {});
 };
 
-export { toApolloClientIFUseQuery };
+export { toApolloClientIFUseQuery, toApolloClientIFUseMutation };
