cfe122cfae235d902ef4b1ec53fd964d7465a617
Yama-Tomo <yam4.tomo@gmail.com>
2021-12-23 10:27:16 +0900

    jestの導入

---
+++ packages/frontend/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  moduleDirectories: ['node_modules', '<rootDir>'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      // TODO: babelでトランスパイルしたほうが速いか試す
+      isolatedModules: true,
+      tsconfig: 'tsconfig.jest.json',
+    },
+  },
+};
