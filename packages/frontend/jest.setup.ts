18bb0b878e9e2d5adcc017b9752b9271e000577d
Yama-Tomo <yam4.tomo@gmail.com>
2021-12-25 09:51:20 +0900

    _app.pages.tsxで参照している環境変数の定義

--- packages/frontend/jest.setup.ts
+++ packages/frontend/jest.setup.ts
@@ -3,9 +3,13 @@ import 'isomorphic-unfetch';
 
 import { mockRouter, server } from '@/test_utils/mocks';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(process as any).browser = true;
+
 process.env = {
   ...process.env,
-  NEXT_PUBLIC_GRAPHQL_URL: 'http://localhost:9999/graphql',
+  NEXT_PUBLIC_GRAPHQL_URL: 'http://localhost:9999/graphql', // dummy
+  NEXT_PUBLIC_WS_GRAPHQL_URL: 'ws://localhost:9999/ws/graphql', // dummy
 };
 
 mockRouter();
