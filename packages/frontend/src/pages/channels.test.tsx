cbab38a30b670303ac31ae0e554109073f36c651
Yama-Tomo <yam4.tomo@gmail.com>
2021-12-25 15:14:44 +0900

    招待されたチャンネルが削除されたときのテストの作成

--- packages/frontend/src/pages/channels.test.tsx
+++ packages/frontend/src/pages/channels.test.tsx
@@ -3,8 +3,12 @@ import Channels from '@/pages/channels.page';
 import { createTestRenderer, publishSubscription } from '@/test_utils/helper';
 import {
   mockSubscriptionExchange,
+  MutationType,
+  myChannelAndProfileQuery,
   newChangeChannelSubscriptionPayload,
+  newChannelWithPersonalizedData,
   newSubscription,
+  newUser,
 } from '@/test_utils/mocks';
 
 const renderer = createTestRenderer(Channels);
@@ -22,7 +26,7 @@ describe('pages/channels', () => {
     expect(await result.findByText(/dm1/)).toBeInTheDocument();
   });
 
-  describe('subscription', () => {
+  describe('他ユーザからのアクション', () => {
     it('チャンネルに招待されたらチャンネル一覧を再描画すること', async () => {
       const [publishable] = mockSubscriptionExchange();
       const result = renderer();
@@ -35,6 +39,41 @@ describe('pages/channels', () => {
       await publishSubscription(publishable, data, 100);
 
       expect(await result.findByText(/new-add-channel/)).toBeInTheDocument();
+
+    it('ほかユーザが作成したチャンネルを削除されたらチャンネル一覧を再描画すること', async () => {
+      const [publishable] = mockSubscriptionExchange();
+
+      const myUser = newUser();
+      const otherUser = newUser();
+      const otherUserCreatedCh = newChannelWithPersonalizedData({
+        name: 'other-user-ch1',
+        ownerId: otherUser.id,
+      });
+      const result = renderer(
+        myChannelAndProfileQuery({
+          channels: [
+            newChannelWithPersonalizedData({ name: 'ch1', ownerId: myUser.id }),
+            otherUserCreatedCh,
+          ],
+          myProfile: myUser,
+        })
+      );
+
+      expect(await result.findByText(/^# ch1$/)).toBeInTheDocument();
+      const otherUserCreatedChMatcher = /^# other-user-ch1$/;
+      expect(await result.findByText(otherUserCreatedChMatcher)).toBeInTheDocument();
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { unReadMessageCount: _, ...rest } = otherUserCreatedCh;
+      const data = newSubscription({
+        changeNotification: newChangeChannelSubscriptionPayload({
+          mutation: MutationType.Deleted,
+          data: { ...rest, __typename: 'Channel' },
+        }),
+      });
+      await publishSubscription(publishable, data);
+
+      expect(result.queryByText(otherUserCreatedChMatcher)).toBeNull();
     });
   });
 });
--- packages/frontend/src/test_utils/mocks/server.ts
+++ packages/frontend/src/test_utils/mocks/server.ts
@@ -2,26 +2,33 @@ import { graphql } from 'msw';
 import { setupServer } from 'msw/node';
 
 import {
+  ChannelWithPersonalizedData,
   MyChannelAndProfileDocument,
   newChannelWithPersonalizedData,
   newUser,
+  User,
 } from './gql_generated_factories';
 
-const handlers = [
-  graphql.query(MyChannelAndProfileDocument, (req, res, ctx) =>
+const myChannelAndProfileQuery = ({
+  channels,
+  myProfile,
+}: { channels?: ChannelWithPersonalizedData[]; myProfile?: User } = {}) => {
+  return graphql.query(MyChannelAndProfileDocument, (req, res, ctx) =>
     res(
       ctx.data({
-        channels: [
+        channels: channels || [
           newChannelWithPersonalizedData({ name: 'ch1' }),
           newChannelWithPersonalizedData({ name: 'ch2' }),
           newChannelWithPersonalizedData({ name: 'dm1', isDM: true }),
         ],
-        myProfile: newUser({ name: 'yamatomo' }),
+        myProfile: myProfile || newUser({ name: 'yamatomo' }),
       })
     )
-  ),
-];
+  );
+};
+
+const handlers = [myChannelAndProfileQuery()];
 
 const server = setupServer(...handlers);
 
-export { server, handlers };
+export { server, handlers, myChannelAndProfileQuery };
