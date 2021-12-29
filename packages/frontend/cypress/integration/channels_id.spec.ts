6d1c4fb1f5504b2057529e8a55cef4916b5c5215
Yama-Tomo <yam4.tomo@gmail.com>
2021-12-29 16:52:08 +0900

    random fail対策

--- packages/frontend/cypress/integration/channels_id.spec.ts
+++ packages/frontend/cypress/integration/channels_id.spec.ts
@@ -7,11 +7,13 @@ describe('/channels/[id]', () => {
     setUserId('test');
 
     cy.visit(`${env.BASE_URL}/channels`);
+    cy.waitForNetworkIdle();
   });
 
   it('メッセージ一覧が表示されること', () => {
     cy.get('a').contains('ch1').click();
     cy.waitForNetworkIdle();
+    cy.contains('message-ch1-14');
     cy.takeScreenshot();
   });
 });
